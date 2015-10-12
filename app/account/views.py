75e4130a2271668cd5cc30b88ec020a6c098e6f5
Egor Orlov <egor.orlov@ostrovok.ru>
2015-10-12 23:29:17 +0300

    cleanup of unused files + login page

--- app/account/views.py
+++ app/account/views.py
@@ -4,11 +4,14 @@ from django.contrib.auth import logout as _logout
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
+from utils import render_to
 
+
+@render_to('login.html')
 def login(request):
     if request.user.is_authenticated():
         return redirect('home')
-    return redirect('social:begin', backend='google-oauth2')
+    return {}
 
 
 def logout(request):
